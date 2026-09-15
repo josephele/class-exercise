28e2d9f8982363990dfb906ad13847a09ad00bcb
josephele <joseph.ele@northeastern.edu>
2026-09-15 15:07:44 -0400

    Complete CSV data checker CLI

---
+++ class-exercise/class2_argparse_demo.py
@@ -0,0 +1,31 @@
+import argparse 
+
+
+
+parser = argparse.ArgumentParser(description="Analyze a data file")
+
+parser.add_argument(
+    "--input", "-i",
+    required=True,
+    help="Path to input CSV file"
+)
+
+# Add named arguments (optional)
+parser.add_argument(
+    "--output", "-o",
+    default="results.txt",
+    help="Output file path"
+)
+
+parser.add_argument(
+    "--verbose", "-v", 
+    action="store_true", 
+    help="Print detailed information"
+    # Note 1: verbose has its default value as False
+    # Note 2: action="store_true" sets this to True.
+)
+
+args = parser.parse_args()
+
+
+### figure out how to get csv in here and then push to github
